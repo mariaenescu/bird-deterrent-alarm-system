9c9dd1a43eca4a930b4a454249af875d2f5f0c65
Maria Enescu <117830404+mariaenescu@users.noreply.github.com>
2025-05-28 20:58:06 +0200

    Replaced delay() with millis() based timing logic

--- arduino_code.ino
+++ arduino_code.ino
@@ -8,19 +8,17 @@ PCF8574 ex(0x20);
 
 #define DEBUG;
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2); // initializez LCDul si specific pinii corespunzatori de pe Arduino
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 // Definirea pinilor pentru LED-ul RGB 
 int red = 0;
-int blue = 1;
-int green = 2;
+int green = 1;
+int blue = 2;
+
 
 // Pinii pentru senzorul ultrasonic
 int trigger = 13;
-int echo1 = 9;
-int echo2 = 10;
-int echo3 = 11;
-int echo4 = 12;
+int echoPins[] = {9, 10, 11, 12};
 
 // Pinul pentru difuzor
 int buzzer = 8;
@@ -29,7 +27,7 @@ int buzzer = 8;
 int lum = A0;
 
 float prev[] = {1000, 1000, 1000, 1000};
-float echo[] = {echo1, echo2, echo3, echo4};
+float distances[4];
 
 // Vector cu frecventele sunetului care va fi redat
 int melody[] = {
@@ -41,13 +39,27 @@ int leds[] = {
   0b100, 0b010, 0b001, 0b101, 0b110, 0b011, 0b111, 0b001, 0b010, 0b100
 };
 
+unsigned long noteMillis = 0;
+unsigned long alarmMillis = 0;
+unsigned long distanceMillis = 0;
+
+const long noteInterval = 200;
+const long alarmInterval = 100;
+const long distanceInterval = 300;
+
+int melodyIndex = 0;
+int alarmIndex = 0;
+int echoIndex = 0;
+int alarmState = 0;
+
+float minDist = 1000; //distanta minima pana la primul obiect detectata de senzori
+int lumPercent = 100; //lumina in procente
 
 void setup() {
   // Seteaz pinii LED-urilor ca iesiri, a senzorul ultrasonic, a  difuzorului ca iesire, senzorului de lumina ca intrare
-  pinMode(echo1, INPUT);
-  pinMode(echo2, INPUT);
-  pinMode(echo3, INPUT);
-  pinMode(echo4, INPUT);
+  for(int i = 0; i < 4; i++) {
+    pinMode(echoPins[i], INPUT);
+  }
 
   pinMode(trigger, OUTPUT);
  
@@ -62,7 +74,7 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(lum, INPUT);
 
-// Initializare numar coloane si randuri LCD
+  // Initializare numar coloane si randuri LCD
   lcd.begin(16,2);
   #ifdef DEBUG
     Serial.begin(9600);
@@ -85,24 +97,35 @@ float distance(int echo) {
 }
 
 
-// Functie pentru redarea unei melodii cu difuzorul
+// // Functie pentru redarea unei melodii cu difuzorul
 void playMelody() {
-  for (int i = 0; i < 10; i++) {
-    tone(buzzer, melody[i]);
-    delay(200); // pune o pauza scurta intre note
+  if(millis() - noteMillis >= noteInterval) {
+    noteMillis = millis();
+    tone(buzzer, melody[melodyIndex]);
+    melodyIndex = (melodyIndex + 1) % 10;
   }
 }
 
 // Functie pentru activarea alarmei complete melodie si LED-ul in toate culorile RGB
 void alarm() {
-  for (int i = 0; i < 10; i++) {
-    tone(buzzer, melody[i]);
-    digitalWrite(ex, red, leds[i] & 0b100 ? HIGH : LOW);
-    delay(100); // pune o pauza scurta intre culorile LED-ului
-    digitalWrite(ex, green, leds[i] & 0b010 ? HIGH : LOW);
-    delay(100); // pune o pauza scurta intre culorile LED-ului
-    digitalWrite(ex, blue, leds[i] & 0b001 ? HIGH : LOW);
-    delay(100); // pune o pauza scurta intre culorile LED-ului
+  if(millis() - alarmMillis >= alarmInterval) {
+    alarmMillis = millis();
+
+    switch(alarmState) {
+      case 0:
+        tone(buzzer, melody[alarmIndex]);
+        digitalWrite(ex, red, leds[alarmIndex] & 0b100 ? HIGH : LOW);
+        break;
+      case 1:
+        digitalWrite(ex, green, leds[alarmIndex] & 0b010 ? HIGH : LOW);
+        break;
+      case 2:
+        digitalWrite(ex, blue, leds[alarmIndex] & 0b001 ? HIGH : LOW);
+        alarmIndex = (alarmIndex + 1) % 10;
+        break;
+    }
+
+    alarmState = (alarmState + 1) % 3;
   }
 }
 
@@ -110,87 +133,95 @@ void alarm() {
 void clear() {
   noTone(buzzer);
   ex.write(0x00);
-  // digitalWrite(ex, red, LOW);
-  // digitalWrite(ex, blue, LOW);
-  // digitalWrite(ex, green, LOW);
 }
 
+
 void loop() {
-  lcd.clear(); // curatare eran LCD la fiecare ciclu
+  unsigned long currentMillis = millis();
 
-  int l = analogRead(lum); // citire valoare lumina
-  int lp = map(l,0,800,0,100);  // mapez valoarea luminii la pentru a lucra cu procent de la 0-100%
-
-#ifdef DEBUG
-    Serial.print("lumina: ");
-    Serial.println(lp);
-#endif
-
-  // Afiseaza procentul luminii pe LCD
-  lcd.setCursor(0,0);
-  lcd.print("L:");
-  lcd.print(lp);
-  lcd.print("%");
-
-  float min = 1000;
-  int index = 0;
-  for(int i = 0; i < 4; i++){
-    float d = distance(echo[i]); // citire valoare distanta
+  if (currentMillis - distanceMillis >= distanceInterval) {
+    distanceMillis = currentMillis;
+    distances[echoIndex] = distance(echoPins[echoIndex]);// citire valoare distanta
     // Pentru distanta negativa, folosim ultima valoare masurată pozitiv
-    if (d <= 0) {
-      d = prev[i];
+    if (distances[echoIndex] <= 0) {
+      distances[echoIndex] = prev[echoIndex];
     } else {
-      prev[i] = d;
+      prev[echoIndex] = distances[echoIndex];
     }
 
-#ifdef DEBUG
+    #ifdef DEBUG
       Serial.print("distanta ");
-      Serial.print(i+1);
+      Serial.print(echoIndex + 1);
       Serial.print(": ");
-      Serial.println(d);
-#endif
-    if (min > d){
-      min = d;
-      index = i;
+      Serial.println(distances[echoIndex]);
+    #endif
+
+    echoIndex++;
+
+    if (echoIndex >= 4) {
+      echoIndex = 0;
+
+      lcd.clear(); // curatare eran LCD la fiecare ciclu
+
+      int l = analogRead(lum); // citire valoare lumina
+      lumPercent = map(l,0,800,0,100);  // mapez valoarea luminii pentru a lucra cu procent de la 0-100%
+
+      #ifdef DEBUG
+        Serial.print("lumina: ");
+        Serial.print(l);
+        Serial.print(" : ");
+        Serial.print(lumPercent);
+        Serial.println("%");
+      #endif
+
+      // Afiseaza procentul luminii pe LCD
+      lcd.setCursor(0,0);
+      lcd.print("L:");
+      lcd.print(lumPercent);
+      lcd.print("%");
+
+      minDist = distances[0];
+      int minIndex = 0;
+      for (int i = 1; i < 4; i++) {
+        if (distances[i] < minDist) {
+          minDist = distances[i];
+          minIndex = i;
+        }
+      }
+
+      if(minDist < 200) {
+        // Afiseaza distanta in metri pe LCD
+        lcd.setCursor(8,0);
+        lcd.print("D:");
+        lcd.print(minDist/100);
+        lcd.print("m");
+
+        // Afisez un mesaj pe LCD
+        lcd.setCursor(1,1);
+        if (minDist < 20) {
+          lcd.print("ALARM");
+        } else {
+          lcd.print("WARNING");
+        }
+
+        lcd.setCursor(12, 1);
+        lcd.print("S:");
+        lcd.print(minIndex + 1);
+      }
+
+      clear();
+
+      if (minDist > 30 && lumPercent <= 25) { 
+        digitalWrite(ex, green, HIGH); 
+      }
     }
-    //delay(300);
-    delay(50);
-  }
-  
-  if(min < 200) {
-    // Afiseaza distanta in metri pe LCD
-    lcd.setCursor(8,0);
-    lcd.print("D:");
-    lcd.print(min/100);
-    lcd.print("m");
-
-    // Afisez un mesaj pe LCD
-    lcd.setCursor(1,1);
-    if (min < 20) {
-      lcd.print("ALARM");
-    } else {
-      lcd.print("WARNING");
-    }
-    
-
-    lcd.setCursor(12, 1);
-    lcd.print("S:");
-    lcd.print(index + 1);
   }
 
-  clear();
-  
-   // Conditii pentru activarea diferitelor componente in functie de distanta si lumina
-  if (min > 30) {
-    if (lp <= 25) {
-      digitalWrite(ex, green, HIGH);
-    } 
-    delay(1000); // pauza de o secunda
-  } else {
-    if (lp > 25) {
+  if (minDist <= 30) {
+    if (lumPercent > 25) {
       playMelody();
     } else {
-      if (min > 10) {
+      if (minDist > 10) {
         digitalWrite(ex, red, HIGH);
         playMelody();
       } else {
@@ -199,4 +230,7 @@ void loop() {
     }
   }
 }
+
+
+
 
